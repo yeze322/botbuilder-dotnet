e604a67f169384e6497b6db918c21fb2d3931f3a
Gaspar Acevedo Zain <43762887+GasparAcevedoZainSouthworks@users.noreply.github.com>
2019-05-15 15:24:39 -0300

    Implement ISlackAdapterOptions (#2)
    * Add botbuilder-slack-adapter project
    
    * Ported ISlackAdapterOptions Interface
    Removed unnecessary using's
    
    * Updated references

--- Botkit/botbuilder-slack-adapter/ISlackAdapterOptions.cs
+++ Botkit/botbuilder-slack-adapter/ISlackAdapterOptions.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace botbuilder_slack_adapter
 {
     interface ISlackAdapterOptions
     {
+        /// <summary>
+        /// Legacy method for validating the origin of incoming webhooks.
+        /// </summary>
+        string VerificationToken { get; }
+
+        /// <summary>
+        /// A token used to validate that incoming webhooks originated with Slack.
+        /// </summary>
+        string ClientSigningSecret { get; }
+
+        /// <summary>
+        /// A token (provided by Slack) for a bot to work on a single workspace.
+        /// </summary>
+        string BotToken { get; }
+
+        /// <summary>
+        /// The oauth client id provided by Slack for multi-team apps.
+        /// </summary>
+        string ClientId { get; }
+
+        /// <summary>
+        /// The oauth client secret provided by Slack for multi-team apps.
+        /// </summary>
+        string ClientSecret { get; }
+
+        /// <summary>
+        /// A an array of scope names that are being requested during the oauth process. Must match the scopes defined at api.slack.com.
+        /// </summary>
+        string[] Scopes { get; }
+
+        /// <summary>
+        /// The URL users will be redirected to after an oauth flow. In most cases, should be `https://<mydomain.com>/install/auth`.
+        /// </summary>
+        string RedirectUri { get; }
+
+        /// <summary>
+        /// A method that receives a Slack team id and returns the bot token associated with that team. Required for multi-team apps.
+        /// </summary>
+        /// <param name="TeamId">Team ID</param>
+        /// <returns></returns>
+        Task<string> GetTokenForTeam(string TeamId);
+
+        /// <summary>
+        /// A method that receives a Slack team id and returns the bot user id associated with that team. Required for multi-team apps.
+        /// </summary>
+        /// <param name="TeamId">Team ID.</param>
+        /// <returns></returns>
+        Task<string> GetBotUserByTeam(string TeamId);
     }
 }
