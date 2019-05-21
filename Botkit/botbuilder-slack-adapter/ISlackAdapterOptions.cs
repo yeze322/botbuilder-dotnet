7e6bc2751757ad89d41de17fd844df41bfdc2827
VictorGrycuk <victor@grycuk.net>
2019-05-21 12:02:14 -0300

    Add MIT licenses

--- Botkit/botbuilder-slack-adapter/ISlackAdapterOptions.cs
+++ Botkit/botbuilder-slack-adapter/ISlackAdapterOptions.cs
@@ -1,3 +1,6 @@
+// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
 using System.Threading.Tasks;
 
 namespace botbuilder_slack_adapter
--- Botkit/botbuilder-slack-adapter/SlackBotWorker.cs
+++ Botkit/botbuilder-slack-adapter/SlackBotWorker.cs
@@ -1,3 +1,6 @@
+// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
 using Microsoft.BotKit;
 using Microsoft.BotKit.Core;
 using System.Threading.Tasks;
