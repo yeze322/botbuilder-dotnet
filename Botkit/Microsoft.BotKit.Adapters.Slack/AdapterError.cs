7956d685fad5bdedb1bafec0d0e8b5f1bd94771c
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-05-29 13:56:19 -0300

    Add AdapterError class

---
+++ Botkit/Microsoft.BotKit.Adapters.Slack/AdapterError.cs
@@ -0,0 +1,16 @@
+// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotKit.Adapters.Slack
+{
+    public class AdapterError
+    {
+        string Name;
+        string Error;
+
+        public AdapterError()
+        {
+
+        }
+    }
+}
