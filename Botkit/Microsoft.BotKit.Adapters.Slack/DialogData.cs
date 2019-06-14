1928ad32dffc7563f225fca7146da1ffaf59f3f3
Cecilia Avila <cecilia.avila@southworks.com>
2019-06-14 14:49:25 -0300

    Fix StyleCop issues in DialogData

--- Botkit/Microsoft.BotKit.Adapters.Slack/DialogData.cs
+++ Botkit/Microsoft.BotKit.Adapters.Slack/DialogData.cs
@@ -5,20 +5,19 @@ using System.Collections.Generic;
 
 namespace Microsoft.BotKit.Adapters.Slack
 {
+    /// <summary>
+    /// Data related to the dialog.
+    /// </summary>
     public class DialogData
     {
-        public string Title { get; set; }
 
-        public string CallbackId { get; set; }
-
-        public string SubmitLabel { get; set; }
-
-        public List<DialogElement> Elements { get; set; }
-
-        public string State { get; set; }
-
-        public bool NotifyOnCancel { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DialogData"/> class.
+        /// </summary>
+        /// <param name="title">title for the Dialog.</param>
+        /// <param name="callback">callback id for the Dialog.</param>
+        /// <param name="submit">submit label for the Dialog.</param>
+        /// <param name="elements">List of elements for the Dialog.</param>
         public DialogData(string title, string callback, string submit, List<DialogElement> elements)
         {
             this.Title = title;
@@ -26,5 +25,35 @@ namespace Microsoft.BotKit.Adapters.Slack
             this.SubmitLabel = submit;
             this.Elements = elements;
         }
+
+        /// <summary>
+        /// Gets or Sets the tittle of the dialog.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the callback Id of the dialog.
+        /// </summary>
+        public string CallbackId { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the submit label of the dialog.
+        /// </summary>
+        public string SubmitLabel { get; set; }
+
+        /// <summary>
+        /// Gets or Sets a list of elements contained by the dialog.
+        /// </summary>
+        public List<DialogElement> Elements { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the state of the dialog.
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the dialog should notify on cancel or not.
+        /// </summary>
+        public bool NotifyOnCancel { get; set; }
     }
 }
