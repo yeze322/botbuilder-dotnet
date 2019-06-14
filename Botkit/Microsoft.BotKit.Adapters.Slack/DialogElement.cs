c9d28ed25098a58f598904d91b67b49ebcb502a5
Cecilia Avila <cecilia.avila@southworks.com>
2019-06-14 14:49:41 -0300

    Fix StyleCop issues in DialogElement

--- Botkit/Microsoft.BotKit.Adapters.Slack/DialogElement.cs
+++ Botkit/Microsoft.BotKit.Adapters.Slack/DialogElement.cs
@@ -5,20 +5,44 @@ using System.Collections.Generic;
 
 namespace Microsoft.BotKit.Adapters.Slack
 {
+    /// <summary>
+    /// Elements related to the Dialog.
+    /// </summary>
     public class DialogElement
     {
+        /// <summary>
+        /// Gets or Sets the label for the dialog element.
+        /// </summary>
         public string Label { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the name for the dialog element.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the value for the dialog element.
+        /// </summary>
         public string Value { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the pair of values for an option List in the dialog element.
+        /// </summary>
         public Dictionary<string, string> OptionList { get; set; }
 
+        /// <summary>
+        /// Gets or Sets options for the dialog element.
+        /// </summary>
         public ISlackAdapterOptions Options { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the type for the dialog element.
+        /// </summary>
         public string Type { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the subtype for the dialog element.
+        /// </summary>
         public string Subtype { get; set; }
     }
 }
