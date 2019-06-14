229a5618fc983225fba1b5b174c0280f9876a0f4
Cecilia Avila <cecilia.avila@southworks.com>
2019-06-14 14:49:59 -0300

    Fix StyleCop issues in ISlackAdapterOptions

--- Botkit/Microsoft.BotKit.Adapters.Slack/ISlackAdapterOptions.cs
+++ Botkit/Microsoft.BotKit.Adapters.Slack/ISlackAdapterOptions.cs
@@ -5,40 +5,43 @@ using System.Threading.Tasks;
 
 namespace Microsoft.BotKit.Adapters.Slack
 {
+    /// <summary>
+    /// Interface for define implementation of the SlackAdapter Options.
+    /// </summary>
     public interface ISlackAdapterOptions
     {
         /// <summary>
-        /// Legacy method for validating the origin of incoming webhooks.
+        /// Gets or Sets the token for validating the origin of incoming webhooks.
         /// </summary>
         string VerificationToken { get; set; }
 
         /// <summary>
-        /// A token used to validate that incoming webhooks originated with Slack.
+        /// Gets or Sets a token used to validate that incoming webhooks originated with Slack.
         /// </summary>
         string ClientSigningSecret { get; set; }
 
         /// <summary>
-        /// A token (provided by Slack) for a bot to work on a single workspace.
+        /// Gets or Sets a token (provided by Slack) for a bot to work on a single workspace.
         /// </summary>
         string BotToken { get; set; }
 
         /// <summary>
-        /// The oauth client id provided by Slack for multi-team apps.
+        /// Gets or Sets the oauth client id provided by Slack for multi-team apps.
         /// </summary>
         string ClientId { get; set; }
 
         /// <summary>
-        /// The oauth client secret provided by Slack for multi-team apps.
+        /// Gets or Sets the oauth client secret provided by Slack for multi-team apps.
         /// </summary>
         string ClientSecret { get; set; }
 
         /// <summary>
-        /// A an array of scope names that are being requested during the oauth process. Must match the scopes defined at api.slack.com.
+        /// Gets or Sets an array of scope names that are being requested during the oauth process. Must match the scopes defined at api.slack.com.
         /// </summary>
         string[] Scopes { get; set; }
 
         /// <summary>
-        /// The URL users will be redirected to after an oauth flow. In most cases, should be `https://<mydomain.com>/install/auth`.
+        /// Gets or Sets the URL users will be redirected to after an oauth flow. In most cases, should be `https://<mydomain.com>/install/auth`.
         /// </summary>
         string RedirectUri { get; set; }
 
@@ -46,14 +49,14 @@ namespace Microsoft.BotKit.Adapters.Slack
         /// A method that receives a Slack team id and returns the bot token associated with that team. Required for multi-team apps.
         /// </summary>
         /// <param name="TeamId">Team ID.</param>
-        /// <returns></returns>
+        /// <returns>The bot token associated with the team.</returns>
         Task<string> GetTokenForTeam(string TeamId);
 
         /// <summary>
         /// A method that receives a Slack team id and returns the bot user id associated with that team. Required for multi-team apps.
         /// </summary>
         /// <param name="TeamId">Team ID.</param>
-        /// <returns></returns>
+        /// <returns>The bot user id associated with that team.</returns>
         Task<string> GetBotUserByTeam(string TeamId);
     }
 }
